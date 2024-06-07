e91476dd00894ee9c1e04d4aa8cb5c4aaa24c780
akashm-18 <akashmurugan338@gmail.com>
2024-06-07 23:50:36 +0530

    "created logo in navbar"

---
+++ components/NavBar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import CustomButton from "./CustomButton"
+
+const NavBar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center
+        sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+            <Image 
+            src="/car-image.png"
+            alt="CarQuest logo"
+            width={118}
+            height={118}
+            className="object-contain"
+            />
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
