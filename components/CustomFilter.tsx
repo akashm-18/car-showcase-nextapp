116821520867a79ebdf7bacebebd902c193b625b
akashm-18 <akashmurugan338@gmail.com>
2024-06-08 13:02:57 +0530

    "started creating searchbar and combobox"

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomFilter = () => {
+  return (
+    <div>
+      Custom Filter
+    </div>
+  )
+}
+
+export default CustomFilter
---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+"use client";
+''
+const SearchBar = () => {
+    const handleSearch = () => {}
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}></form>
+  )
+}
+
+export default SearchBar
