294e63443498e4e9c01a45c38be578d50c51a970
akashm-18 <akashmurugan338@gmail.com>
2024-06-10 22:07:12 +0530

    "Completed comboBox section"

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({manufacturer , setManufacturer} : searchManufacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
             <Combobox.Button className="absolute top-[14px]">
                 <Image  
@@ -64,7 +64,20 @@ const SearchManufacturer = ({manufacturer , setManufacturer} : searchManufacture
                                 className={({active}) => `relative search-manufacturer__option
                                 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                                 >
-                                {item}
+                                {({ selected , active }) => (
+                                    <>
+                                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                                        {item}
+                                    </span>
+                                    {
+                                        selected ? (
+                                            <span className={`absolute inset-y-0 left-0 flex items-center pl-3
+                                                ${active ? 'text-white' : 'text-teal-600'}`}>
+                                            </span>
+                                        ) : null
+                                    }
+                                    </>
+                                )}
                                 </Combobox.Option>
                             ))
                         )}
