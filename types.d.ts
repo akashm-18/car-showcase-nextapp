dde16e9860144fe6fed7eb60ee4a10ddf561168a
akashm-18 <akashmurugan338@gmail.com>
2024-06-12 00:54:58 +0530

    "Completed car card component"

--- types.d.ts
+++ types.d.ts
@@ -6,6 +6,9 @@ export interface CustomButtonProps {
     containerStyles? : string;
     handleClick? : MouseEventHandler<HTMLButtonElement>;
     btnType? : "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface searchManufacturerProps {
